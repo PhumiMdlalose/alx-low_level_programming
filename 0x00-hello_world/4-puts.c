91a81fb65786b2e4f47aed0097f7b8cccb609199
PhumiMdlalose <princessdlamini69@gmail.com>
2023-02-16 12:17:28 -0800

    puts fuction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-printf ("Programming is like building a multilingual puzzle"\n);
+#include <studio.h>
+/*
+ * main - print a line of code using puts
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
