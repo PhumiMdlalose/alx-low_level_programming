680432ac954f790bcd9beb8d0f7432db2232de6d
PhumiMdlalose <princessdlamini69@gmail.com>
2023-02-16 21:59:45 -0800

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,21 @@
 #include <stdio.h>
+/**
+ * main - print out sizes of datat types in C
+ * code by Phumelele Mdlalose
+ * Return: 0
+*/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
 
+	printf("Size of a char: 1%lu byte(S)\n", (unsigned long)size of(a));
+	printf("Size of a int: 1%lu byte(S)\n", (unsigned long)size of(b));
+	printf("Size of a long int: 1%lu byte(S)\n", (unsigned long)size of(c));
+	printf("Size of a long long int: 1%lu byte(S)\n", (unsigned long)size of(d));
+	printf("Size of a float: 1%lu byte(S)\n", (unsigned long)size of(f));
+	return (0);
+}
