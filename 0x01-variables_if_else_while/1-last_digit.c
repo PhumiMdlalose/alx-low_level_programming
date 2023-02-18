9e3a1d702cf5421bf5aa6a0d23dda23b27f48a41
PhumiMdlalose <princessdlamini69@gmail.com>
2023-02-18 13:39:02 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <time.h>
 /**
  * main - main block
- * Description - This program will assign a random number to the variable n
+ * Description - Get a random number and check its last digit, compare it with 5
  *Return: 0
  */
 int main(void)
@@ -29,5 +29,3 @@ int main(void)
 	}
 	return (0);
 }
-
-
