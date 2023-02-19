f11bd144d3a42ede8e05b70f1c03e2a759efb9cc
PhumiMdlalose <princessdlamini69@gmail.com>
2023-02-19 11:20:11 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base
+ * 10 starting from 0, followd by a new line
+ * Retun: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+		printf("\n");
+	retun (0);
+}
