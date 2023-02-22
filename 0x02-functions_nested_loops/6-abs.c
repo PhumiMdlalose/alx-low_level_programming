1e41ac081d3207f1438f521335ae6976c9c30f51
PhumiMdlalose <princessdlamini69@gmail.com>
2023-02-22 08:13:43 -0800

    task 8

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+ /**
+ * _abs - Computed the abs val of an int
+ * @c: The number to be computed.
+ * Return: Absolute value of the int.
+ */
+
+int _abs(int)
+{
+	if (c < 0)
+	{
+	int abs_val;
+
+	abs_val = c * -1;
+	return (abs_val);
+	}
+	return (c);
+}
