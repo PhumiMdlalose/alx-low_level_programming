d4f63b4dd7e3960988959bc7c66b126c254b916a
PhumiMdlalose <princessdlamini69@gmail.com>
2023-03-11 07:39:20 -0800

    My first static library

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+ /**
+ * _abs - Computes the abs val of an int
+ * @n: The int in question
+ *
+ * Return: the abs of the int.
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the char c to stdout
+ * @c: The char to print
+ *
+ * Return: In success 1
+ * On error, -1 is returuned, and errno is set appropiately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
