c9e57418339913f864029359b3f3c30c9f0f0bdb
PhumiMdlalose <princessdlamini69@gmail.com>
2023-03-12 23:11:44 -0700

    argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * Write a program that prints its name, followed by a new line.
+ * main - write a program that prints its name, followed by a new line.
+ * @argc: counts the number of elements
+ * @argv: passes argument to the string.
  * Return: new name, if program is renamed
  */
 
-int main (int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", *argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
