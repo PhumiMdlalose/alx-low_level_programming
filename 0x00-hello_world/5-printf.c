0dfc8cb748f099b2f73364e6864f73e52227cf61
PhumiMdlalose <princessdlamimini69@gmail.com>
2023-07-13 12:34:04 -0700

    C hello world tasks

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-printf("with proper grammar, but the outcome is a piece of art,\n");
+#include <stdio.h>
+
+/**
+ * main - write a function using the printf function
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
