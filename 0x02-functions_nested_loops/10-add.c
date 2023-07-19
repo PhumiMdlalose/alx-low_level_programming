a820c0e0bb9571823e484ef4c0d30e2d7e7b4bcb
PhumiMdlalose <princessdlamimini69@gmail.com>
2023-07-19 00:29:19 -0700

    0x02 tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two intergers
+ * @x: first int to be added
+ * @y: second int to be added
+ * Return: int result
+ */
+
+int add(int x, int y)
+{
+	int result;
+
+	result = x + y;
+
+	return (result);
+}
