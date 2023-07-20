50ae990200458a2dc067dffb5effa5f6d5506d5f
PhumiMdlalose <princessdlamimini69@gmail.com>
2023-07-20 10:55:25 -0700

    0x02 tasks

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer.
+ * @n: pos or neg int
+ * Return: n
+ */
+
+int _abs(int n)
+{
+	if (n > 0 || n < 0)
+	{
+		return (n);
+	}
+	else
+		return (0);
+}
