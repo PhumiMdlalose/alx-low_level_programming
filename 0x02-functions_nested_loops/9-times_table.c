69c0d0a18d5905cca3d3c1b10ac96841f9eb2ffa
PhumiMdlalose <princessdlamimini69@gmail.com>
2023-07-23 17:11:33 -0700

    0x02 tasks

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ */
+
+void times_table(void)
+{
+	int n, row, sum, dig_1, dig_2;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (n = 0; n <= row; n++)
+
+		sum = row * n;
+		dig_1 = ((sum / 10) + '0');
+		dig_2 = ((sum % 10) + '0');
+
+		if (sum <= 9)
+		{
+			_putchar(dig_2);
+		}
+		else
+		{
+			_putchar(dig_1);
+		}
+		_putchar(',');
+		_putchar(' ');
+		_putchar('\n');
+	}
+}
